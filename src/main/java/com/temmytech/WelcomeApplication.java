34f67809a5f2c8a0534c14028c5bdc41f682c901
temmy.techie <temmy.tech01@outlook.com>
2023-06-28 01:37:13 +0200

    initial commit

---
+++ src/main/java/com/temmytech/WelcomeApplication.java
@@ -0,0 +1,15 @@
+package com.temmytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author TemmyTechie
+ */
+
+@SpringBootApplication
+public class WelcomeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WelcomeApplication.class, args);
+    }
+}
